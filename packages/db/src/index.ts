85ab61c2f93b6175d4f3874adf9f889f79540aed
Turbobot <turbobot@vercel.com>
2025-07-14 10:43:15 +0530

    feat(create-turbo): apply official-starter transform

--- packages/db/src/index.ts
+++
@@ -1,5 +0,0 @@
-import { PrismaClient } from '../generated/prisma/client'
- const prismaClient = new PrismaClient()
-
- export default prismaClient;
-// use `prisma` in your application to read and write data in your DBs
